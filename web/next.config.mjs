c1384c995a2bf05d5e5c96427ce487944ea97228
Syedain Iqbal Shigri <sishigree@gmail.com>
2026-02-18 13:56:40 +0500

    fix: outputFileTracingRoot for Vercel monorepo

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,3 +1,8 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,6 +11,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  outputFileTracingRoot: path.join(__dirname, '../../'),
 }
 
 export default nextConfig
